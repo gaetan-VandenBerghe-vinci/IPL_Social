09cfbfee44253e1c0c251758be40a09c7e1aa199
Gaetan Van den Berghe <gaetan.vandenberghe@student.uclouvain.be>
2025-01-17 16:16:06 +0100

    First test return false

--- spec/passwordCheckertTest.js
+++ spec/passwordCheckertTest.js
@@ -4,4 +4,8 @@ describe("Test for at least 8 charactere", function (){
     it("Should return true cause 8 char", function(){
         expect(true).toBe(true)
     })
+
+    it("Should return false cause < 8 char", function (){
+        expect(false).toBe(false);
+    })
 })
